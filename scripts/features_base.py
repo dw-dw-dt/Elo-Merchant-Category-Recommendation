b0e32ccf50488e7f07ae1ad995da6dda5750b9b4
Daiya Mita <18240230+dw-dw-dt@users.noreply.github.com>
2019-01-15 21:00:21 +0900

    update

---
+++ scripts/features_base.py
@@ -0,0 +1,85 @@
+import argparse
+import inspect
+import feather
+import pandas as pd
+import re
+from abc import ABCMeta, abstractmethod
+from pathlib import Path
+import os
+import sys
+cwd = os.getcwd()
+sys.path.append(cwd.replace('/scripts',''))
+from utils import timer
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--force', '-f', action='store_true', help='Overwrite existing files'
+    )
+    return parser.parse_args()
+
+
+def get_features(namespace):
+    for k, v in namespace.items():
+        if inspect.isclass(v) and issubclass(v, Feature) \
+                and not inspect.isabstract(v):
+            yield v()
+
+
+def generate_features(namespace, overwrite):
+    for f in get_features(namespace):
+        #if f.train_path.exists() and f.test_path.exists() and not overwrite:
+        if f.df_path.exists() and not overwrite:
+            print(f.name, 'was skipped')
+        else:
+            f.run().save()
+
+
+class Feature(metaclass=ABCMeta):
+    prefix = ''
+    suffix = ''
+    dir = '.'
+
+    def __init__(self):
+        if self.__class__.__name__.isupper():
+            self.name = self.__class__.__name__.lower()
+        else:
+            self.name = re.sub(
+                "([A-Z])",
+                lambda x: "_" + x.group(1).lower(), self.__class__.__name__
+            ).lstrip('_')
+
+        #self.train = pd.DataFrame()
+        #self.test = pd.DataFrame()
+        self.df = pd.DataFrame()
+        #self.train_path = Path(self.dir) / f'{self.name}_train.feather'
+        #self.test_path = Path(self.dir) / f'{self.name}_test.feather'
+        self.df_path = Path(self.dir) / f'{self.name}_df.feather'
+
+    def run(self):
+        with timer(self.name):
+            self.create_features()
+            prefix = self.prefix + '_' if self.prefix else ''
+            suffix = '_' + self.suffix if self.suffix else ''
+            #self.train.columns = prefix + self.train.columns + suffix
+            #self.test.columns = prefix + self.test.columns + suffix
+            self.df.columns = prefix + self.df.columns + suffix
+        return self
+
+    @abstractmethod
+    def create_features(self):
+        raise NotImplementedError
+
+
+    def save(self):
+        #self.train.to_feather(str(self.train_path))
+        #self.test.to_feather(str(self.test_path))
+        self.df.to_feather(str(self.df_path))
+
+
+    def load(self):
+        self.df = feather.read_dataframe(str(self.df_path))
+        #self.train = pd.read_feather(str(self.train_path))
+        #self.test = pd.read_feather(str(self.test_path))
+
