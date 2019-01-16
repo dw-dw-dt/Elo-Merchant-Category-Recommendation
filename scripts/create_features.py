c21e81f23fb9e46a58c11539e0bc4a58edd6e102
Daiya Mita <18240230+dw-dw-dt@users.noreply.github.com>
2019-01-16 23:53:01 +0900

    特徴量のfeather化まで完了

--- scripts/create_features.py
+++ scripts/create_features.py
@@ -79,7 +79,7 @@ class Train_test(Feature):
         self.df = df
 
 
-class Historical_transaction(Feature):
+class Historical_transactions(Feature):
     def create_features(self):
         #hist_df = pd.read_csv('../input/historical_transactions.csv', nrows=num_rows)
         hist_df = feather.read_dataframe('./data/input/historical_transactions.feather')
@@ -196,6 +196,196 @@ class Historical_transaction(Feature):
         hist_df = hist_df.reset_index()
 
         self.df = hist_df
+
+
+class New_merchant_transactions(Feature):
+    def create_features(self):
+        # load csv
+        #new_merchant_df = pd.read_csv('../input/new_merchant_transactions.csv', nrows=num_rows)
+        new_merchant_df = feather.read_dataframe('./data/input/new_merchant_transactions.feather')
+
+        # fillna
+        new_merchant_df['category_2'].fillna(1.0,inplace=True)
+        new_merchant_df['category_3'].fillna('A',inplace=True)
+        new_merchant_df['merchant_id'].fillna('M_ID_00a6ca8a8a',inplace=True)
+        new_merchant_df['installments'].replace(-1, np.nan,inplace=True)
+        new_merchant_df['installments'].replace(999, np.nan,inplace=True)
+
+        # Y/Nのカラムを1-0へ変換
+        new_merchant_df['authorized_flag'] = new_merchant_df['authorized_flag'].map({'Y': 1, 'N': 0}).astype(int)
+        new_merchant_df['category_1'] = new_merchant_df['category_1'].map({'Y': 1, 'N': 0}).astype(int)
+        new_merchant_df['category_3'] = new_merchant_df['category_3'].map({'A':0, 'B':1, 'C':2}).astype(int)
+
+        # datetime features
+        new_merchant_df['purchase_date'] = pd.to_datetime(new_merchant_df['purchase_date'])
+    #    new_merchant_df['year'] = new_merchant_df['purchase_date'].dt.year
+        new_merchant_df['month'] = new_merchant_df['purchase_date'].dt.month
+        new_merchant_df['day'] = new_merchant_df['purchase_date'].dt.day
+        new_merchant_df['hour'] = new_merchant_df['purchase_date'].dt.hour
+        new_merchant_df['weekofyear'] = new_merchant_df['purchase_date'].dt.weekofyear
+        new_merchant_df['weekday'] = new_merchant_df['purchase_date'].dt.weekday
+        new_merchant_df['weekend'] = (new_merchant_df['purchase_date'].dt.weekday >=5).astype(int)
+
+        # additional features
+        new_merchant_df['price'] = new_merchant_df['purchase_amount'] / new_merchant_df['installments']
+
+        #ブラジルの休日
+        cal = Brazil()
+    #    new_merchant_df['is_holiday'] = new_merchant_df['purchase_date'].dt.date.apply(cal.is_holiday).astype(int)
+
+        # 購入日からイベント日までの経過日数
+        #Christmas : December 25 2017
+        new_merchant_df['Christmas_Day_2017']=(pd.to_datetime('2017-12-25')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
+        #Mothers Day: May 14 2017
+    #    new_merchant_df['Mothers_Day_2017']=(pd.to_datetime('2017-06-04')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
+        #fathers day: August 13 2017
+        new_merchant_df['fathers_day_2017']=(pd.to_datetime('2017-08-13')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
+        #Childrens day: October 12 2017
+        new_merchant_df['Children_day_2017']=(pd.to_datetime('2017-10-12')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
+        #Valentine's Day : 12th June, 2017
+    #    new_merchant_df['Valentine_Day_2017']=(pd.to_datetime('2017-06-12')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
+        #Black Friday : 24th November 2017
+        new_merchant_df['Black_Friday_2017']=(pd.to_datetime('2017-11-24') - new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
+
+        #2018
+        #Mothers Day: May 13 2018
+        new_merchant_df['Mothers_Day_2018']=(pd.to_datetime('2018-05-13')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
+
+        new_merchant_df['month_diff'] = ((datetime.datetime.today() - new_merchant_df['purchase_date']).dt.days)//30
+        new_merchant_df['month_diff'] += new_merchant_df['month_lag']
+
+        # additional features
+        new_merchant_df['duration'] = new_merchant_df['purchase_amount']*new_merchant_df['month_diff']
+        new_merchant_df['amount_month_ratio'] = new_merchant_df['purchase_amount']/new_merchant_df['month_diff']
+
+        # memory usage削減
+        #new_merchant_df = reduce_mem_usage(new_merchant_df)
+
+        col_unique =['subsector_id', 'merchant_id', 'merchant_category_id']
+        col_seas = ['hour', 'weekofyear', 'weekday', 'day']
+
+        aggs = {}
+        for col in col_unique:
+            aggs[col] = ['nunique']
+
+        for col in col_seas:
+            aggs[col] = ['nunique', 'mean', 'min', 'max']
+
+        aggs['purchase_amount'] = ['sum','max','min','mean','var','skew']
+        aggs['installments'] = ['sum','max','mean','var','skew']
+        aggs['purchase_date'] = ['max','min']
+        aggs['month_lag'] = ['max','min','mean','var','skew']
+        aggs['month_diff'] = ['mean','var','skew']
+    #    aggs['authorized_flag'] = ['mean']
+        aggs['weekend'] = ['mean']
+        aggs['month'] = ['mean', 'min', 'max']
+        aggs['category_1'] = ['mean', 'min']
+        aggs['category_2'] = ['mean', 'min']
+        aggs['category_3'] = ['mean', 'min']
+        aggs['card_id'] = ['size','count']
+    #    aggs['is_holiday'] = ['mean']
+        aggs['price'] = ['mean','max','min','var','skew']
+        aggs['Christmas_Day_2017'] = ['mean']
+    #    aggs['Mothers_Day_2017'] = ['mean']
+        aggs['fathers_day_2017'] = ['mean']
+        aggs['Children_day_2017'] = ['mean']
+    #    aggs['Valentine_Day_2017'] = ['mean']
+        aggs['Black_Friday_2017'] = ['mean']
+        aggs['Mothers_Day_2018'] = ['mean']
+        aggs['duration']=['mean','min','max','var','skew']
+        aggs['amount_month_ratio']=['mean','min','max','var','skew']
+
+        for col in ['category_2','category_3']:
+            new_merchant_df[col+'_mean'] = new_merchant_df.groupby([col])['purchase_amount'].transform('mean')
+            new_merchant_df[col+'_min'] = new_merchant_df.groupby([col])['purchase_amount'].transform('min')
+            new_merchant_df[col+'_max'] = new_merchant_df.groupby([col])['purchase_amount'].transform('max')
+            new_merchant_df[col+'_sum'] = new_merchant_df.groupby([col])['purchase_amount'].transform('sum')
+            aggs[col+'_mean'] = ['mean']
+
+        new_merchant_df = new_merchant_df.reset_index().groupby('card_id').agg(aggs)
+
+        # カラム名の変更
+        new_merchant_df.columns = pd.Index([e[0] + "_" + e[1] for e in new_merchant_df.columns.tolist()])
+        new_merchant_df.columns = ['new_'+ c for c in new_merchant_df.columns]
+
+        new_merchant_df['new_purchase_date_diff'] = (new_merchant_df['new_purchase_date_max']-new_merchant_df['new_purchase_date_min']).dt.days
+        new_merchant_df['new_purchase_date_average'] = new_merchant_df['new_purchase_date_diff']/new_merchant_df['new_card_id_size']
+        new_merchant_df['new_purchase_date_uptonow'] = (datetime.datetime.today()-new_merchant_df['new_purchase_date_max']).dt.days
+        new_merchant_df['new_purchase_date_uptomin'] = (datetime.datetime.today()-new_merchant_df['new_purchase_date_min']).dt.days
+
+        # memory usage削減
+        #new_merchant_df = reduce_mem_usage(new_merchant_df)
+
+        new_merchant_df = new_merchant_df.reset_index()
+
+        self.df = new_merchant_df
+
+
+def load_all():
+    df = feather.read_dataframe('./features/train_test_df.feather')
+    hist_df = feather.read_dataframe('./features/historical_transactions_df.feather')
+    df = pd.merge(df, hist_df, on='card_id', how='outer')
+    new_merchant_df =feather.read_dataframe('./features/new_merchant_transactions_df.feather')
+    df = pd.merge(df, new_merchant_df, on='card_id', how='outer')
+    return df
+
+class Additional_features(Feature):
+    def create_features(self):
+        df = load_all()
+        df['hist_first_buy'] = (df['hist_purchase_date_min'] - df['first_active_month']).dt.days
+        df['hist_last_buy'] = (df['hist_purchase_date_max'] - df['first_active_month']).dt.days
+        df['new_first_buy'] = (df['new_purchase_date_min'] - df['first_active_month']).dt.days
+        df['new_last_buy'] = (df['new_purchase_date_max'] - df['first_active_month']).dt.days
+
+        date_features=['hist_purchase_date_max','hist_purchase_date_min',
+                    'new_purchase_date_max', 'new_purchase_date_min']
+
+        for f in date_features:
+            df[f] = df[f].astype(np.int64) * 1e-9
+
+        df['card_id_total'] = df['new_card_id_size']+df['hist_card_id_size']
+        df['card_id_cnt_total'] = df['new_card_id_count']+df['hist_card_id_count']
+        df['card_id_cnt_ratio'] = df['new_card_id_count']/df['hist_card_id_count']
+        df['purchase_amount_total'] = df['new_purchase_amount_sum']+df['hist_purchase_amount_sum']
+        df['purchase_amount_mean'] = df['new_purchase_amount_mean']+df['hist_purchase_amount_mean']
+        df['purchase_amount_max'] = df['new_purchase_amount_max']+df['hist_purchase_amount_max']
+        df['purchase_amount_min'] = df['new_purchase_amount_min']+df['hist_purchase_amount_min']
+        df['purchase_amount_var'] = df['new_purchase_amount_var']+df['hist_purchase_amount_var']
+        df['purchase_amount_skew'] = df['new_purchase_amount_skew']+df['hist_purchase_amount_skew']
+        df['purchase_amount_ratio'] = df['new_purchase_amount_sum']/df['hist_purchase_amount_sum']
+        df['month_diff_mean'] = df['new_month_diff_mean']+df['hist_month_diff_mean']
+        df['month_diff_ratio'] = df['new_month_diff_mean']/df['hist_month_diff_mean']
+    #    df['month_diff_max'] = df['new_month_diff_max']+df['hist_month_diff_max']
+    #    df['month_diff_min'] = df['new_month_diff_min']+df['hist_month_diff_min']
+        df['month_lag_mean'] = df['new_month_lag_mean']+df['hist_month_lag_mean']
+        df['month_lag_max'] = df['new_month_lag_max']+df['hist_month_lag_max']
+        df['month_lag_min'] = df['new_month_lag_min']+df['hist_month_lag_min']
+        df['category_1_mean'] = df['new_category_1_mean']+df['hist_category_1_mean']
+    #    df['category_1_min'] = df['new_category_1_min']+df['hist_category_1_min']
+        df['installments_total'] = df['new_installments_sum']+df['hist_installments_sum']
+        df['installments_mean'] = df['new_installments_mean']+df['hist_installments_mean']
+        df['installments_max'] = df['new_installments_max']+df['hist_installments_max']
+        df['installments_ratio'] = df['new_installments_sum']/df['hist_installments_sum']
+        df['price_total'] = df['purchase_amount_total'] / df['installments_total']
+        df['price_mean'] = df['purchase_amount_mean'] / df['installments_mean']
+        df['price_max'] = df['purchase_amount_max'] / df['installments_max']
+        df['price_var'] = df['new_price_var'] + df['hist_price_var']
+        df['price_skew'] = df['new_price_skew'] + df['hist_price_skew']
+        df['duration_mean'] = df['new_duration_mean']+df['hist_duration_mean']
+        df['duration_min'] = df['new_duration_min']+df['hist_duration_min']
+        df['duration_max'] = df['new_duration_max']+df['hist_duration_max']
+        df['duration_var'] = df['new_duration_var']+df['hist_duration_var']
+        df['duration_skew'] = df['new_duration_skew']+df['hist_duration_skew']
+        df['amount_month_ratio_mean']=df['new_amount_month_ratio_mean']+df['hist_amount_month_ratio_mean']
+        df['amount_month_ratio_min']=df['new_amount_month_ratio_min']+df['hist_amount_month_ratio_min']
+        df['amount_month_ratio_max']=df['new_amount_month_ratio_max']+df['hist_amount_month_ratio_max']
+        df['amount_month_ratio_var']=df['new_amount_month_ratio_var']+df['hist_amount_month_ratio_var']
+        df['amount_month_ratio_skew']=df['new_amount_month_ratio_skew']+df['hist_amount_month_ratio_skew']
+        df['new_CLV'] = df['new_card_id_count'] * df['new_purchase_amount_sum'] / df['new_month_diff_mean']
+        df['hist_CLV'] = df['hist_card_id_count'] * df['hist_purchase_amount_sum'] / df['hist_month_diff_mean']
+        df['CLV_ratio'] = df['new_CLV'] / df['hist_CLV']
+
+        self.df = df
 
 
 if __name__ == '__main__':
