ce50238120a97c386155b1c59ec44ecb21c0155c
dWt <daiyamaxus@gmail.com>
2019-01-17 18:34:32 +0900

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,7 +3,26 @@ import pandas as pd
 import numpy as np
 import feather
 from contextlib import contextmanager
+import logging
+from lightgbm.callback import _format_eval_result
 
+
+def log_best(model, metric):
+    logging.debug(model.best_iteration)
+    logging.debug(model.best_score['valid_0'][metric])
+
+
+def log_evaluation(logger, period=1, show_stdv=True, level=logging.DEBUG):
+    def _callback(env):
+        if period > 0 and env.evaluation_result_list \
+                and (env.iteration + 1) % period == 0:
+            result = '\t'.join([
+                _format_eval_result(x, show_stdv)
+                for x in env.evaluation_result_list
+            ])
+            logger.log(level, '[{}]\t{}'.format(env.iteration + 1, result))
+    _callback.order = 10
+    return _callback
 
 @contextmanager
 def timer(name):
