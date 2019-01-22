0705c2fa32c2a376d706a219f3b92bc3bad711a0
MitsuruFujiwara <fujiwara52jp@gmail.com>
2019-01-22 19:52:25 +0900

    LB#67

--- user02/create_features.py
+++ user02/create_features.py
@@ -53,7 +53,7 @@ class Traintest(Feature):
     #    df['weekofyear'] = df['first_active_month'].dt.weekofyear.fillna(0).astype(int).astype(object)
         df['quarter'] = df['first_active_month'].dt.quarter
     #    df['month_year'] = df['month'].astype(str)+'_'+df['year'].astype(str)
-        df['elapsed_time'] = (datetime.datetime.today() - df['first_active_month']).dt.days
+        df['elapsed_time'] = (pd.to_datetime('2019-01-18') - df['first_active_month']).dt.days
 
         df['days_feature1'] = df['elapsed_time'] * df['feature_1']
         df['days_feature2'] = df['elapsed_time'] * df['feature_2']
@@ -106,7 +106,7 @@ class Historical_transactions(Feature):
 
         # datetime features
         hist_df['purchase_date'] = pd.to_datetime(hist_df['purchase_date'])
-        hist_df['year'] = hist_df['purchase_date'].dt.year
+    #    hist_df['year'] = hist_df['purchase_date'].dt.year
         hist_df['month'] = hist_df['purchase_date'].dt.month
         hist_df['day'] = hist_df['purchase_date'].dt.day
         hist_df['hour'] = hist_df['purchase_date'].dt.hour
@@ -139,18 +139,18 @@ class Historical_transactions(Feature):
         #Mothers Day: May 13 2018
         hist_df['Mothers_Day_2018']=(pd.to_datetime('2018-05-13')-hist_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
 
-        hist_df['month_diff'] = ((pd.to_datetime('2019-01-01') - hist_df['purchase_date']).dt.days)//30
+        hist_df['month_diff'] = ((pd.to_datetime('2019-01-18') - hist_df['purchase_date']).dt.days)//30
         hist_df['month_diff'] += hist_df['month_lag']
 
         # additional features
         hist_df['duration'] = hist_df['purchase_amount']*hist_df['month_diff']
         hist_df['amount_month_ratio'] = hist_df['purchase_amount']/hist_df['month_diff']
 
-        # memory usage削減 <この部分で謎のエラーが出るのでコメントアウト pyarrow.lib.ArrowNotImplementedError: halffloat
-        #hist_df = reduce_mem_usage(hist_df)
+        # memory usage削減
+#        hist_df = reduce_mem_usage(hist_df)
 
         col_unique =['subsector_id', 'merchant_id', 'merchant_category_id']
-        col_seas = ['month', 'hour', 'weekofyear']
+        col_seas = ['month', 'hour', 'weekofyear', 'weekday', 'day']
 
         aggs = {}
         for col in col_unique:
@@ -164,7 +164,7 @@ class Historical_transactions(Feature):
         aggs['purchase_date'] = ['max','min']
         aggs['month_lag'] = ['max','min','mean','var','skew']
         aggs['month_diff'] = ['max','min','mean','var','skew']
-        aggs['authorized_flag'] = ['mean', 'sum']
+        aggs['authorized_flag'] = ['mean']
         aggs['weekend'] = ['mean'] # overwrite
         aggs['weekday'] = ['mean'] # overwrit
         aggs['day'] = ['nunique', 'mean', 'min'] # overwrite
@@ -199,8 +199,8 @@ class Historical_transactions(Feature):
 
         hist_df['hist_purchase_date_diff'] = (hist_df['hist_purchase_date_max']-hist_df['hist_purchase_date_min']).dt.days
         hist_df['hist_purchase_date_average'] = hist_df['hist_purchase_date_diff']/hist_df['hist_card_id_size']
-        hist_df['hist_purchase_date_uptonow'] = (datetime.datetime.today()-hist_df['hist_purchase_date_max']).dt.days
-        hist_df['hist_purchase_date_uptomin'] = (datetime.datetime.today()-hist_df['hist_purchase_date_min']).dt.days
+        hist_df['hist_purchase_date_uptonow'] = (pd.to_datetime('2019-01-18')-hist_df['hist_purchase_date_max']).dt.days
+        hist_df['hist_purchase_date_uptomin'] = (pd.to_datetime('2019-01-18')-hist_df['hist_purchase_date_min']).dt.days
 
         #hist_df = hist_df.reset_index()
 
@@ -244,7 +244,7 @@ class New_merchant_transactions(Feature):
 
         # datetime features
         new_merchant_df['purchase_date'] = pd.to_datetime(new_merchant_df['purchase_date'])
-        new_merchant_df['year'] = new_merchant_df['purchase_date'].dt.year
+    #    new_merchant_df['year'] = new_merchant_df['purchase_date'].dt.year
         new_merchant_df['month'] = new_merchant_df['purchase_date'].dt.month
         new_merchant_df['day'] = new_merchant_df['purchase_date'].dt.day
         new_merchant_df['hour'] = new_merchant_df['purchase_date'].dt.hour
@@ -257,19 +257,19 @@ class New_merchant_transactions(Feature):
 
         #ブラジルの休日
         cal = Brazil()
-        new_merchant_df['is_holiday'] = new_merchant_df['purchase_date'].dt.date.apply(cal.is_holiday).astype(int)
+    #    new_merchant_df['is_holiday'] = new_merchant_df['purchase_date'].dt.date.apply(cal.is_holiday).astype(int)
 
         # 購入日からイベント日までの経過日数
         #Christmas : December 25 2017
         new_merchant_df['Christmas_Day_2017']=(pd.to_datetime('2017-12-25')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
         #Mothers Day: May 14 2017
-        new_merchant_df['Mothers_Day_2017']=(pd.to_datetime('2017-06-04')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
+    #    new_merchant_df['Mothers_Day_2017']=(pd.to_datetime('2017-06-04')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
         #fathers day: August 13 2017
-        new_merchant_df['fathers_day_2017']=(pd.to_datetime('2017-08-13')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
+    #    new_merchant_df['fathers_day_2017']=(pd.to_datetime('2017-08-13')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
         #Childrens day: October 12 2017
         new_merchant_df['Children_day_2017']=(pd.to_datetime('2017-10-12')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
         #Valentine's Day : 12th June, 2017
-        new_merchant_df['Valentine_Day_2017']=(pd.to_datetime('2017-06-12')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
+    #    new_merchant_df['Valentine_Day_2017']=(pd.to_datetime('2017-06-12')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
         #Black Friday : 24th November 2017
         new_merchant_df['Black_Friday_2017']=(pd.to_datetime('2017-11-24') - new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
 
@@ -277,7 +277,7 @@ class New_merchant_transactions(Feature):
         #Mothers Day: May 13 2018
         new_merchant_df['Mothers_Day_2018']=(pd.to_datetime('2018-05-13')-new_merchant_df['purchase_date']).dt.days.apply(lambda x: x if x > 0 and x < 100 else 0)
 
-        new_merchant_df['month_diff'] = ((pd.to_datetime('2019-01-01') - new_merchant_df['purchase_date']).dt.days)//30
+        new_merchant_df['month_diff'] = ((pd.to_datetime('2019-01-18') - new_merchant_df['purchase_date']).dt.days)//30
         new_merchant_df['month_diff'] += new_merchant_df['month_lag']
 
         # additional features
@@ -285,10 +285,10 @@ class New_merchant_transactions(Feature):
         new_merchant_df['amount_month_ratio'] = new_merchant_df['purchase_amount']/new_merchant_df['month_diff']
 
         # memory usage削減
-        #new_merchant_df = reduce_mem_usage(new_merchant_df)
+#        new_merchant_df = reduce_mem_usage(new_merchant_df)
 
         col_unique =['subsector_id', 'merchant_id', 'merchant_category_id']
-        col_seas = ['hour', 'weekofyear', 'weekday', 'day']
+        col_seas = ['month', 'hour', 'weekofyear', 'weekday', 'day']
 
         aggs = {}
         for col in col_unique:
@@ -302,7 +302,7 @@ class New_merchant_transactions(Feature):
         aggs['purchase_date'] = ['max','min']
         aggs['month_lag'] = ['max','min','mean','var','skew']
         aggs['month_diff'] = ['mean','var','skew']
-        aggs['authorized_flag'] = ['mean']
+    #    aggs['authorized_flag'] = ['mean']
         aggs['weekend'] = ['mean']
         aggs['month'] = ['mean', 'min', 'max']
         aggs['weekday'] = ['mean', 'min', 'max']
@@ -310,13 +310,13 @@ class New_merchant_transactions(Feature):
         aggs['category_2'] = ['mean']
         aggs['category_3'] = ['mean']
         aggs['card_id'] = ['size','count']
-        aggs['is_holiday'] = [ 'mean']
+    #    aggs['is_holiday'] = [ 'mean']
         aggs['price'] = ['mean','max','min','var']
         aggs['Christmas_Day_2017'] = ['mean']
-        aggs['Mothers_Day_2017'] = ['mean']
-        aggs['fathers_day_2017'] = ['mean']
+    #    aggs['Mothers_Day_2017'] = ['mean']
+    #    aggs['fathers_day_2017'] = ['mean']
         aggs['Children_day_2017'] = ['mean']
-        aggs['Valentine_Day_2017'] = ['mean']
+    #    aggs['Valentine_Day_2017'] = ['mean']
         aggs['Black_Friday_2017'] = ['mean']
         aggs['Mothers_Day_2018'] = ['mean']
         aggs['duration']=['mean','min','max','var','skew']
@@ -337,8 +337,8 @@ class New_merchant_transactions(Feature):
 
         new_merchant_df['new_purchase_date_diff'] = (new_merchant_df['new_purchase_date_max']-new_merchant_df['new_purchase_date_min']).dt.days
         new_merchant_df['new_purchase_date_average'] = new_merchant_df['new_purchase_date_diff']/new_merchant_df['new_card_id_size']
-        new_merchant_df['new_purchase_date_uptonow'] = (datetime.datetime.today()-new_merchant_df['new_purchase_date_max']).dt.days
-        new_merchant_df['new_purchase_date_uptomin'] = (datetime.datetime.today()-new_merchant_df['new_purchase_date_min']).dt.days
+        new_merchant_df['new_purchase_date_uptonow'] = (pd.to_datetime('2019-01-18')-new_merchant_df['new_purchase_date_max']).dt.days
+        new_merchant_df['new_purchase_date_uptomin'] = (pd.to_datetime('2019-01-18')-new_merchant_df['new_purchase_date_min']).dt.days
 
         # memory usage削減
         #new_merchant_df = reduce_mem_usage(new_merchant_df)
@@ -373,7 +373,7 @@ class Additional_features(Feature):
         df['new_last_buy'] = (df['new_purchase_date_max'] - df['first_active_month']).dt.days
 
         date_features=['hist_purchase_date_max','hist_purchase_date_min',
-                    'new_purchase_date_max', 'new_purchase_date_min']
+                       'new_purchase_date_max', 'new_purchase_date_min']
 
         for f in date_features:
             df[f] = df[f].astype(np.int64) * 1e-9
@@ -388,13 +388,13 @@ class Additional_features(Feature):
         df['purchase_amount_ratio'] = df['new_purchase_amount_sum']/df['hist_purchase_amount_sum']
         df['month_diff_mean'] = df['new_month_diff_mean']+df['hist_month_diff_mean']
         df['month_diff_ratio'] = df['new_month_diff_mean']/df['hist_month_diff_mean']
-        df['month_diff_max'] = df['new_month_diff_max']+df['hist_month_diff_max']
-        df['month_diff_min'] = df['new_month_diff_min']+df['hist_month_diff_min']
+    #    df['month_diff_max'] = df['new_month_diff_max']+df['hist_month_diff_max']
+    #    df['month_diff_min'] = df['new_month_diff_min']+df['hist_month_diff_min']
         df['month_lag_mean'] = df['new_month_lag_mean']+df['hist_month_lag_mean']
         df['month_lag_max'] = df['new_month_lag_max']+df['hist_month_lag_max']
         df['month_lag_min'] = df['new_month_lag_min']+df['hist_month_lag_min']
         df['category_1_mean'] = df['new_category_1_mean']+df['hist_category_1_mean']
-        df['category_1_min'] = df['new_category_1_min']+df['hist_category_1_min']
+    #    df['category_1_min'] = df['new_category_1_min']+df['hist_category_1_min']
         df['installments_total'] = df['new_installments_sum']+df['hist_installments_sum']
         df['installments_mean'] = df['new_installments_mean']+df['hist_installments_mean']
         df['installments_max'] = df['new_installments_max']+df['hist_installments_max']
